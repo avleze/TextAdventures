46f71eee372ad20cccd03d9fddd1ef6781135755
juanrodicio <juan.antonio_decai10@hotmail.com>
2016-11-22 11:45:20 +0100

    Added new class Map

---
+++ Map.java
@@ -0,0 +1,41 @@
+
+/**
+ * Map class
+ * @author Juan Antonio Rodicio López
+ */
+public class Map {
+    
+    private Room[][] rooms;
+    private final int width;
+    private final int height;
+    
+    Map(Room[][] rooms,int width,int height){
+        this.rooms = rooms;
+        this.width = width;
+        this.height = height;
+        
+    }
+    
+    Room[][] getRooms()
+    {
+        return rooms;
+    }
+    
+    int getWidth()
+    {
+        return width;
+    }
+    
+    int getHeight()
+    {
+        return height;
+    }
+    
+    void setRooms(Room[][] roomsToSet)
+    {
+        this.rooms = roomsToSet;
+    }
+    
+}
+
+
