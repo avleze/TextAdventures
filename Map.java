64759ff53edab6fd61f6365e0da599435c508aa0
Antonio Vélez Estévez <antonio.velezestevez@alum.uca.es>
2016-11-22 17:45:14 +0100

    Changing code style according to java style guidelines.

--- Map.java
+++ Map.java
@@ -1,40 +1,37 @@
 
 /**
  * Map class
+ *
  * @author Juan Antonio Rodicio López
  */
 public class Map {
-    
+
     private Room[][] rooms;
     private final int width;
     private final int height;
-    
-    Map(Room[][] rooms,int width,int height){
+
+    Map(Room[][] rooms, int width, int height) {
         this.rooms = rooms;
         this.width = width;
         this.height = height;
     }
-    
-    Room[][] getRooms()
-    {
+
+    Room[][] getRooms() {
         return rooms;
     }
-    
-    int getWidth()
-    {
+
+    int getWidth() {
         return width;
     }
-    
-    int getHeight()
-    {
+
+    int getHeight() {
         return height;
     }
-    
-    void setRooms(Room[][] rooms)
-    {
+
+    void setRooms(Room[][] rooms) {
         this.rooms = rooms;
     }
-    
+
 }
 
 
