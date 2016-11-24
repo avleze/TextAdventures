1db44f7bd848cda78f2e290a010198b92dd1dea2
juanrodicio <juan.antonio_decai10@hotmail.com>
2016-11-24 12:31:17 +0100

    Added DecisionEngine class

---
+++ TextAdventures/DecisionEngine.java
@@ -0,0 +1,39 @@
+import TextAdventures.Output.OutputManager;
+import TextAdventures.Input.InputManager;
+
+/**
+ * DecisionEngine Class
+ * @author Juan Antonio Rodicio López
+ */
+public class DecisionEngine {
+    
+    PlayerCharacter playerCharacter;
+    Map map;
+    OutputManager output;
+    InputManager input;
+    
+    DecisionEngine(PlayerCharacter playerCharacter, Map map){
+        this.playerCharacter = playerCharacter;
+        this.map = map;
+    }
+    
+    void run(){
+        
+        boolean gameOver = false;
+        
+        while(!gameOver && playerCharacter.isAlive()){
+        
+        Room room = map.getRoom(playerCharacter.getXPosition(), 
+                                    playerCharacter.getYPosition);
+        
+        output.show(playerCharacter,room,room.getActions());
+        
+        room.getAction(input.getInput()).run();
+        
+        if(!playerCharacter.isAlive() /* || Player reach the treasure room*/)
+            gameOver = false;
+        
+        }
+        
+    }
+}
