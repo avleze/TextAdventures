be0525f219b51fd42be650928102d08209a19b37
Luis Rozo <luisgonzagarozo@gmail.com>
2016-12-01 11:21:59 +0100

    Add classes Room, Action (& sub-classes) v1.1
    Version 1.1 of this classes is due to the changes suffered by the UML diagram; changing some parameters of the classes and actual/formal ones of inherited methods.

---
+++ TextAdventures/Action.java
@@ -0,0 +1,22 @@
+package TextAdventures;
+
+/**
+ * This abstract class represents an action of the game.
+ * @author Luis Gonzaga Rozo Bueno
+ * @version 1.1
+ */
+public abstract class Action {
+	private String description;
+	private PlayerCharacter player;
+	
+	public Action(String description,PlayerCharacter player) {
+		this.description = description;
+		this.player = player;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	abstract void run();
+}
---
+++ TextAdventures/MovementAction.java
@@ -0,0 +1,29 @@
+package TextAdventures;
+
+/**
+ * This class represents a movement action.
+ * @author Luis Gonzaga Rozo Bueno
+ * @version 1.1
+ */
+public class MovementAction extends Action{
+	private enum movementType{
+		UP,DOWN,LEFT,RIGHT
+	}
+
+	/**
+	 * Constructor
+	 * @param description This initializes the parameter of the super-class.
+	 * @param player Initializes the super-class parameter.
+	 */
+	public MovementAction(String description,PlayerCharacter player,movementType tipo) {
+		super(description,player);
+	}
+
+	@Override
+	/**
+	 * Executes the action
+	 */
+	public void run() {
+		
+	}
+}
