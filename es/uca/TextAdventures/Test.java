6643d72d4ea4e9cfca0475505c6d0ef6bd5a1759
manuelrdsg <manuelrds@gmail.com>
2017-01-30 12:44:17 +0100

    Bugfixing

--- es/uca/TextAdventures/Test.java
+++ es/uca/TextAdventures/Test.java
@@ -29,52 +29,11 @@ public class  Test {
         inventory.add(sword);
         inventory.add(superPotion);
 
-        PlayerCharacter player = new PlayerCharacter("Juan", 1, 100, inventory, 10, 0, 0);
+        PlayerCharacter player = new PlayerCharacter("Heroe Anonimo", 1, 100, inventory, 10, 0, 0);
 
         DecisionEngine decisionEngine = new DecisionEngine(player);
 
         decisionEngine.run();
-
-        /*mapLoader = new MapLoader(player);
-
-        mapa = mapLoader.loadFromFile("map.xml");
-
-        System.out.println("Tamaño: " + mapa.getWidth() + "x" + mapa.getHeight());
-        Room[][] room = mapa.getRooms();
-
-        System.out.println(room[0][0].getMessage().getCaption());
-        System.out.println(room[0][0].getMessage().getMessage());
-        for(Action action : room[0][0].getActions()){
-            System.out.println(action.getDescription());
-        }
-        System.out.println(room[0][0].getEnemy().getName());
-        
-        System.out.println(room[0][1].getMessage().getCaption());
-        System.out.println(room[0][1].getMessage().getMessage());
-        System.out.println(room[0][1].getActions().size());
-        System.out.println(room[0][1].getEnemy().getName());
-        
-        System.out.println(room[1][0].getMessage().getCaption());
-        System.out.println(room[1][0].getMessage().getMessage());
-        System.out.println(room[1][0].getActions().size());
-        System.out.println(room[1][0].getEnemy().getName());
-        
-        player.setYPosition(1);
-        
-        mockDB.saveCurrentGame(player);
-        
-        PlayerCharacter savedPlayer = mockDB.loadLastGame();
-        
-        System.out.println("Name: "+savedPlayer.getName());
-        System.out.println("ID: "+savedPlayer.getId());
-        System.out.println("Health Points: "+savedPlayer.getHealthPoints());
-        for(Item item : savedPlayer.getInventory()){
-            System.out.println(item.getId());
-        }
-        
-        System.out.println("Base Damage: "+savedPlayer.getBaseDamage());
-        System.out.println("X position: "+savedPlayer.getXPosition());
-        System.out.println("Y position: "+savedPlayer.getYPosition());*/
     }
 
 }
